293bee5fe227795c53c193f7567a38efd6aa4ba4
orbitartprint <orbitartprint@gmail.com>
2025-09-26 22:40:43 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,12 @@
 import Navigation from "@/components/ui/navigation";
+import { useNavigate, Link } from "react-router-dom";
 import Footer from "@/components/ui/footer";
 import HeroSection from "@/components/sections/hero-section";
 import VideoSection from "@/components/sections/video-section";
 
 const Index = () => {
+  const navigate = useNavigate();
+  useEffect(() => {window.scrollTo(0, 0);}, []);
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
