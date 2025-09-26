248b9b7ed899d2f0b1300bb4cc01d762e9f675cd
orbitartprint <orbitartprint@gmail.com>
2025-09-26 22:45:03 +0200

    Update FreeResources.tsx

--- src/pages/FreeResources.tsx
+++ src/pages/FreeResources.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
