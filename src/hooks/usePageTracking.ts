a61c929811117eb8e2aee278fa8baf1c869f03f8
orbitartprint <orbitartprint@gmail.com>
2025-10-19 22:20:18 +0200

    Create usePageTracking.ts

---
+++ src/hooks/usePageTracking.ts
@@ -0,0 +1,19 @@
+// src/hooks/usePageTracking.ts
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+/**
+ * Automatically tracks page views in Google Analytics (GA4)
+ * when the route changes in your React app.
+ */
+export default function usePageTracking() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("config", "G-Y60WQZCGGY", {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+}
