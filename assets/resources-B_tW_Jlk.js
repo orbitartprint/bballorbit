8559bd8df755754a552b6dcb5aec2ffab98cfd3e
orbitartprint <orbitartprint@users.noreply.github.com>
2025-10-23 20:22:02 +0000

    deploy: 78021c0d5a644150856e494dc4f20e7c75c3683d

---
+++ assets/resources-B_tW_Jlk.js
@@ -0,0 +1,6 @@
+import{c as e}from"./index-xZ9PAuCj.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("Download",[["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["polyline",{points:"7 10 12 15 17 10",key:"2ggqvy"}],["line",{x1:"12",x2:"12",y1:"15",y2:"3",key:"1vk2je"}]]),n=[{id:"zoom-action",title:"Zoom Action",description:"A complete guide to the Zoom Action.",image:"/lovable-uploads/zoom-action-pdf.webp",category:"Offense",type:"Free",link:"https://guide.bballorbit.com/zoom",filePath:"/pdfs/zoom-action.pdf"},{id:"defense-toolkit",title:"Run And Jump Defense",description:"A complete guide to the Run and Jump Defense.",image:"/lovable-uploads/run-and-jump-pdf.webp",category:"Defense",type:"Free",link:"https://guide.bballorbit.com/runandjump",filePath:"/pdfs/run-and-jump-defense.pdf"}],i=["All","Shooting","Offense","Defense","Conditioning","Tools"];export{t as D,i as c,n as r};
