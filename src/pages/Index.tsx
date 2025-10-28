aeecb17df842385a5dde66d9b5ed96f292586cd8
orbitartprint <orbitartprint@gmail.com>
2025-10-28 23:19:14 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,10 +8,33 @@ import MoreVideos from "@/components/sections/more-videos";
 import Testimonials from "@/components/sections/testimonials";
 import BlogPromotion from "@/components/sections/blog-promotion";
 import LatestBlogPosts from "@/components/sections/latest-blog-posts";
+import { Helmet } from "react-helmet";
 
 const Index = () => {
   useEffect(() => {window.scrollTo(0, 0);}, []);
   return (
+      <script type="application/ld+json">
+      {JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        name: "Basketball Orbit",
+        url: "https://www.bballorbit.com",
+        logo: "https://www.bballorbit.com/logo.png",
+        sameAs: [
+          "https://www.youtube.com/@BasketballOrbit",
+          "https://www.instagram.com/basketball_orbit"
+        ]
+      })}
+    </script>
+  
+    <script type="application/ld+json">
+      {JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        name: "BballOrbit.com - Modern Drills. Smarter Coaching. Better Players.",
+        url: "https://www.bballorbit.com"
+      })}
+    </script>
     <div className="min-h-screen bg-background">
       <Navigation />
       <main>
